5771e175d7b902aa200301790397d4c41ee2b804
Chabod Maxime <77924317+ChabodMaxime@users.noreply.github.com>
2021-04-13 11:33:32 +0200

    Update get_serial_port.ps1

--- get_serial_port.ps1
+++ get_serial_port.ps1
@@ -1,4 +1,14 @@
 $port= new-Object System.IO.Ports.SerialPort COM3,115200,None,8,one
-$port.Open()
-$port.ReadLine()
+Try
+{
+ $port.Open()
+ $port.ReadLine()
+}
+Catch
+{
+ Write-Host "Error"
+ Break
+}
+
 $port.Close()
+
